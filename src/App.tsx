a212df0508d73c8ec7dccdcf45f9f754cba52a1c
OgneFF <wyrik9324@gmail.com>
2025-05-16 22:32:21 +0300

    add custom slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Slider } from "./Slider/Slider";
+
+const images = [
+  { id: 1, image: "/img1.jpg", title: "Первая" },
+  { id: 2, image: "/img2.jpg", title: "Вторая" },
+  { id: 3, image: "/img3.jpg", title: "Третья" },
+  { id: 4, image: "/img4.jpg", title: "Четвёртая" },
+];
 
 export const App: React.FC = () => {
   const [user, setUser] = useState<Telegram.WebAppUser | null>(null);
@@ -26,6 +34,7 @@ export const App: React.FC = () => {
     <>
       <button onClick={handlerButton}> ПРИВЕТ </button>
       <span> {user?.last_name}</span>
+      <Slider slides={images} />
     </>
   );
 };
