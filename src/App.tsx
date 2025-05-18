42e71c128a79e48c33683895cf2281f4d6a54520
OgneFF <wyrik9324@gmail.com>
2025-05-18 23:11:00 +0300

    change slider from desktop

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import { isMobile, isDesktop } from "react-device-detect";
 
 const images = [
   { id: 1, image: "/img1.jpeg", title: "Первая" },
-  { id: 2, image: "/img1.jpeg", title: "Вторая" },
+  { id: 2, image: "/img2.jpg", title: "Вторая" },
   { id: 3, image: "/img1.jpeg", title: "Третья" },
-  { id: 4, image: "/img1.jpeg", title: "Четвёртая" },
+  { id: 4, image: "/img2.jpg", title: "Четвёртая" },
   { id: 5, image: "/img1.jpeg", title: "Пять" },
 ];
 
