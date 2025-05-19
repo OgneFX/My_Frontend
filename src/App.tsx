6805fbd08c5bdd55b15fe3bb96ce08c85b353694
OgneFF <wyrik9324@gmail.com>
2025-05-19 21:15:32 +0300

    fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import { Slider } from "./MobileSlider/MobileSlider";
 import { DesktopSlider } from "./DesktopSlider/DesktopSlider";
