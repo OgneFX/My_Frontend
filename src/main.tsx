50bb2336d84a7214cf05c859fa0b7082f16ae94d
OgneFF <wyrik9324@gmail.com>
2025-05-20 17:15:23 +0300

    change slider

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,10 @@ import { postEvent } from "@telegram-apps/sdk";
 import "./slyles/global.scss";
 import { App } from "./App";
 
-// postEvent("web_app_expand");
-// postEvent("web_app_setup_main_button", { is_visible: false });
-// postEvent("web_app_setup_swipe_behavior", { allow_vertical_swipe: false });
-// postEvent("web_app_ready");
+postEvent("web_app_expand");
+postEvent("web_app_setup_main_button", { is_visible: false });
+postEvent("web_app_setup_swipe_behavior", { allow_vertical_swipe: false });
+postEvent("web_app_ready");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
