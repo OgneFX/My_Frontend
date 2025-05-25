9bfcdb6fd462c59cb3dd22578a4df257f4103eaf
OgneFF <wyrik9324@gmail.com>
2025-05-25 19:52:47 +0300

    online test

--- src/customHooks/useTelegramAuth.tsx
+++ src/customHooks/useTelegramAuth.tsx
@@ -3,7 +3,10 @@ import axios from "axios";
 export const useTelegramAuth = () => {
   const registerUser = async (userData: any) => {
     try {
-      const res = await axios.post("/api/user", userData);
+      const res = await axios.post(
+        "https://my-backend-cwvb.onrender.com/api/user",
+        userData
+      );
       return res.data;
     } catch (e) {
       console.error("Registration failed:", e);
