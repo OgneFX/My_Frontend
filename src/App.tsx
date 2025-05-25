f231bc84ba44d474c50803a2f48e1f538a789beb
OgneFF <wyrik9324@gmail.com>
2025-05-25 20:47:19 +0300

    online test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const images = [
   { id: 1, image: "/img1.jpeg", title: "Первая" },
   { id: 2, image: "/img2.jpg", title: "Вторая" },
   { id: 3, image: "/img1.jpeg", title: "Третья" },
-  { id: 4, image: "/img2.jpg", title: "Четвёртая" },
+  { id: 4, image: "/img2.jpg", title: "Четвёsртая" },
   { id: 5, image: "/img1.jpeg", title: "Пятьd" },
 ]; //временное решение
 
