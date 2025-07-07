f487aa1372bf1a7131973ea36c41cb9854da0ab2
OgneFF <wyrik9324@gmail.com>
2025-07-07 23:01:05 +0300

    Change styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { postEvent } from "@telegram-apps/sdk";
+import { isMobile } from "react-device-detect";
 import "./slyles/global.scss";
 import { App } from "./App";
 
-try {
-  postEvent("web_app_expand");
-} catch (err) {
-  console.error("Ошибка при попытке развернуть WebApp:", err);
+postEvent("web_app_expand");
+if (isMobile) {
+  postEvent("web_app_request_fullscreen");
 }
-postEvent("web_app_request_fullscreen");
 
 postEvent("web_app_setup_main_button", { is_visible: false });
 postEvent("web_app_setup_swipe_behavior", { allow_vertical_swipe: false });
