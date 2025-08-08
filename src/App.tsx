4779851b3b7a48dd38f6ac1005138bc1d78b0840
OgneFF <wyrik9324@gmail.com>
2025-08-08 16:03:11 +0300

    some change

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ export const App: React.FC = () => {
   useEffect(() => {
     const checkRegistration = async () => {
       try {
+        console.log(userObj?.tgWebAppData?.user?.id);
         const response = await axios.get(
           "https://my-backend-cwvb.onrender.com/api/user/check",
           {
